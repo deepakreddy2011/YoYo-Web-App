cd1bdc3c5467815e39636f04f808f39af9675cae
Deepak Tumukunta <dtumukun@jcp.com>
2020-10-13 21:52:55 +0530

    Add project files.

---
+++ YoYo-Web-App/wwwroot/js/YoYoTest.js
@@ -0,0 +1,14 @@
+﻿$(document).load(function () {
+
+});
+
+function showLoader() {
+    $('#playButton').toggle();
+    var canvas = $('#circle').circleProgress({
+        value: 0.1,
+        size: 150,
+        fill: {
+            gradient: ['#ffcccb', '#ff5f43']
+        }
+    });
+}
